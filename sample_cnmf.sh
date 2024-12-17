c2f3a3be97c861d3cb91e88e8ca4ef4476bcc196
evansjr5 <james.r.evans@vanderbilt.edu>
2024-12-17 09:04:47 -0600

    Update sample_cnmf.sh

--- sample_cnmf.sh
+++ sample_cnmf.sh
@@ -2,10 +2,10 @@ data_dir=/home/james/data/immune_exclusion_data/seperate_sample_data
 output_dir=/home/james/data/cNMF_out/per_sample_cnmf
 for file in "$data_dir"/*; do
     echo "$file"
-    name="$file"
-    echo "$name:"
     sample_prefix=$(basename "$file" | cut -d'/' -f1 | cut -d'.' -f1)
     echo "$sample_prefix"
+    sample_name=${sample_prefix}_cNMF
+    echo "$sample_name"
     
     #cnmf prepare "$file" \
     #--output-dir "$output_dir" \
