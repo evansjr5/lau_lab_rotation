4f2b0d990edb793165ce5810060df1a0ae7811f0
evansjr5 <james.r.evans@vanderbilt.edu>
2024-12-17 09:33:10 -0600

    Update sample_cnmf.sh

--- sample_cnmf.sh
+++ sample_cnmf.sh
@@ -1,27 +1,33 @@
 data_dir=/home/james/data/immune_exclusion_data/seperate_sample_data
 output_dir=/home/james/data/cNMF_out/per_sample_cnmf
 
-#for file in "$data_dir"/*; do
- #   echo "$file"
-  #  sample_prefix=$(basename "$file" | cut -d'/' -f1 | cut -d'.' -f1)
-   # sample_name=${sample_prefix}_cNMF
-    
-    #cnmf prepare "$file" \
-    #--output-dir "$output_dir" \
-    #--name "$sample_name" \
-    #-k 8 9 10 11 14 16 18 19 20 22 25 26 27 28 30 31 33 35 \
-    #-n 30 \
-    #-j 1
-    #echo "Finished"
-#done
-
 for file in "$data_dir"/*; do
+    echo "$file"
     sample_prefix=$(basename "$file" | cut -d'/' -f1 | cut -d'.' -f1)
     sample_name=${sample_prefix}_cNMF
+    
+    cnmf prepare "$file" \
+    --output-dir "$output_dir" \
+    --name "$sample_name" \
+    -k 10 \
+    -n 30 \
+    -j 1
+
     cnmf factorize \
     --output-dir "$output_dir" \
     --name "$sample_name" \
-    --n-jobs 5 \
+    --n-jobs 1 \
     --worker-index 0
+
+    cnmf combine \
+    --output-dir "$output_dir" \
+    --name "$sample_name" \
+    -k 10
+
+    cnmf k_selection_plot \
+    --output-dir "$output_dir" \
+    --name "$sample_name"
+
+    echo "Finished prepare, foactorize, combine, and k-selection plot"
+
 done
-    
