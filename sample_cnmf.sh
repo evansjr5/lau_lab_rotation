6570ce26ae69362bde1ee9f0eaded70cd2b46de5
evansjr5 <james.r.evans@vanderbilt.edu>
2024-12-17 10:31:40 -0600

    Update sample_cnmf.sh

--- sample_cnmf.sh
+++ sample_cnmf.sh
@@ -6,28 +6,33 @@ for file in "$data_dir"/*; do
     sample_prefix=$(basename "$file" | cut -d'/' -f1 | cut -d'.' -f1)
     sample_name=${sample_prefix}_cNMF
     
+    echo "Starting prepare command:"
     cnmf prepare "$file" \
     --output-dir "$output_dir" \
     --name "$sample_name" \
-    -k 10 \
+    -k 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 \
     -n 30 \
     -j 1
+    echo "Finished prepare command!"
 
+    echo "Starting Factorization:"
     cnmf factorize \
     --output-dir "$output_dir" \
     --name "$sample_name" \
     --n-jobs 1 \
     --worker-index 0
-
+    echo "Finished Factorization!"
+    
+    echo "Combine and plot:"
     cnmf combine \
     --output-dir "$output_dir" \
     --name "$sample_name" \
-    -k 10
+    -k 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33
 
     cnmf k_selection_plot \
     --output-dir "$output_dir" \
     --name "$sample_name"
 
-    echo "Finished prepare, foactorize, combine, and k-selection plot"
+    echo "Finished prepare, factorize, combine, and k-selection plot"
 
 done
